3675b35bc5ee52b776e1d158960eb83d7790218f
Amad Khan <amadkhan.999@gmail.com>
2019-05-03 16:14:03 +0500

    dynamic values for the shapes

--- civixa/src/components/Civixa.vue
+++ civixa/src/components/Civixa.vue
@@ -8,11 +8,49 @@
       </b-col>
       <b-col>
       <div class="mt-5">
-        
-          <b-button size="lg" variant="success" @click='shapeSelector("square")'>Square</b-button>
-          <b-button size="lg" variant="success" @click='shapeSelector("circle")'>Circle</b-button>
-          <b-button size="lg" variant="danger" @click='shapeSelector("clear")'>Clear</b-button>
-        
+          <b-button size="md" variant="success" @click='shapeSelector("square")'>Square</b-button>
+          <b-button size="md" variant="success" @click='shapeSelector("circle")'>Circle</b-button>
+          <b-button size="md" variant="" @click='shapeSelector("clear")'>Clear Selection</b-button>
+      </div>
+      <div v-if="square">
+        <b-form-group
+        id="input-group-1"
+        label="Height"
+        label-for="input-height"
+      >
+        <b-form-input
+          id="input-height"
+          v-model="form.height"
+          type="number"
+          placeholder="Height"
+        ></b-form-input>
+      </b-form-group>
+        <b-form-group
+          id="input-group-1"
+          label="Width"
+          label-for="input-width"
+        >
+          <b-form-input
+            id="input-width"
+            v-model="form.width"
+            type="number"
+            placeholder="Width"
+          ></b-form-input>
+        </b-form-group>
+      </div>
+      <div v-if="circle">
+        <b-form-group
+          id="input-group-1"
+          label="Radius"
+          label-for="input-radius"
+        >
+          <b-form-input
+            id="input-radius"
+            v-model="form.radius"
+            type="number"
+            placeholder="Radius"
+          ></b-form-input>
+        </b-form-group>
       </div>
       </b-col>
     </b-row>
@@ -24,34 +62,43 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
+      square: false,
+      circle: false,
+      form: {
+        height: 100,
+        width: 100,
+        radius: 100
+      },
       c: '',
       ctx: '',
-      img: '',
-      workingShape: ''
+      img: ''
     }
   },
   mounted () {
     this.c = document.getElementById('myCanvas')
     this.ctx = this.c.getContext('2d')
     this.img = new Image()
-    this.workingShape = ''
     this.img.src = 'https://cdn.pixabay.com/photo/2016/09/01/10/23/image-1635747_960_720.jpg'
     setTimeout(() => {
       this.ctx.drawImage(this.img, 0, 0)
-    }, 100)
+    }, 400)
     this.c.addEventListener('click', (evt) => {
       var mousePos = this.getMousePos(this.c, evt)
-      console.log(mousePos.x + ',' + mousePos.y)
+      // console.log(mousePos.x + ',' + mousePos.y)
       this.drawShapes(mousePos.x, mousePos.y)
     }, false)
   },
   methods: {
     shapeSelector (shape) {
-      if (shape !== 'clear') {
-        this.workingShape = shape
-      }
-      else {
-        this.workingShape = ''
+      if (shape === 'square') {
+        this.square = true
+        this.circle = false
+      } else if (shape === 'circle') {
+        this.circle = true
+        this.square = false
+      } else {
+        this.circle = false
+        this.square = false
       }
     },
     getMousePos (canvas, evt) {
@@ -62,16 +109,14 @@ export default {
       }
     },
     drawShapes (x, y) {
-      if (this.workingShape === 'square') {
-        this.ctx.rect(x - 100, y - 100, 200, 200)
+      if (this.square) {
+        this.ctx.rect(x - (this.form.width / 2), y - (this.form.height / 2), this.form.width, this.form.height)
         this.ctx.stroke()
-      }
-      else if (this.workingShape === 'circle') {
+      } else if (this.circle) {
         this.ctx.beginPath()
-        this.ctx.arc(x, y, 100, 0, 2 * Math.PI)
+        this.ctx.arc(x, y, this.form.radius, 0, 2 * Math.PI)
         this.ctx.stroke()
-      }
-      else {
+      } else {
 
       }
     }
