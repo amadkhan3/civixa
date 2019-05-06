a8c26d1aad70f08ad38efe3eb641f7b1961c912e
Amad Khan <amadkhan.999@gmail.com>
2019-05-06 16:30:52 +0500

    dragable squares

--- civixa/src/components/Civixa.vue
+++ civixa/src/components/Civixa.vue
@@ -11,8 +11,9 @@
           <b-button size="md" variant="success" @click='shapeSelector("square")'>Square</b-button>
           <b-button size="md" variant="success" @click='shapeSelector("circle")'>Circle</b-button>
           <b-button size="md" variant="" @click='shapeSelector("clear")'>Clear Selection</b-button>
+          <!-- <b-button size="md" variant="" @click='removeShape'>Remove</b-button> -->
       </div>
-      <div v-if="square">
+      <!-- <div v-if="square">
         <b-form-group
         id="input-group-1"
         label="Height"
@@ -51,7 +52,7 @@
             placeholder="Radius"
           ></b-form-input>
         </b-form-group>
-      </div>
+      </div> -->
       </b-col>
     </b-row>
   </div>
@@ -62,6 +63,8 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
+      rect: {},
+      drag: false,
       square: false,
       circle: false,
       form: {
@@ -71,24 +74,73 @@ export default {
       },
       c: '',
       ctx: '',
-      img: ''
+      img: '',
+      shapesCreated: {
+        rectangle: [],
+        circle: []
+      }
     }
   },
   mounted () {
     this.c = document.getElementById('myCanvas')
     this.ctx = this.c.getContext('2d')
     this.img = new Image()
-    this.img.src = 'https://cdn.pixabay.com/photo/2016/09/01/10/23/image-1635747_960_720.jpg'
+    this.img.src = 'https://wonderfulengineering.com/wp-content/uploads/2016/01/Desktop-Wallpaper-4.jpg'
     setTimeout(() => {
       this.ctx.drawImage(this.img, 0, 0)
+      this.shapesCreated.rectangle.forEach(s => {
+        this.drawRectange(s.x, s.y, s.width, s.height)
+      })
     }, 400)
-    this.c.addEventListener('click', (evt) => {
-      var mousePos = this.getMousePos(this.c, evt)
-      // console.log(mousePos.x + ',' + mousePos.y)
-      this.drawShapes(mousePos.x, mousePos.y)
-    }, false)
+    this.c.addEventListener('mousedown', this.mouseDown, false)
+    this.c.addEventListener('mouseup', this.mouseUp, false)
+    this.c.addEventListener('mousemove', this.mouseMove, false)
+    // this.c.addEventListener('click', (evt) => {
+    //   var mousePos = this.getMousePos(this.c, evt)
+    //   this.saveShapes(mousePos.x, mousePos.y)
+    // }, false)
   },
   methods: {
+    mouseDown (e) {
+      this.rect.startX = e.pageX - this.c.offsetLeft
+      this.rect.startY = e.pageY - this.c.offsetTop
+      this.drag = true
+    },
+    mouseUp () {
+      this.drag = false
+      // console.log(this.rect.startX, this.rect.startY, this.rect.w, this.rect.h)
+      if(this.square)
+      this.saveShapes(this.rect.startX, this.rect.startY, this.rect.w, this.rect.h)
+    },
+    mouseMove (e) {
+      if (this.drag && this.square) {
+        this.rect.w = (e.pageX - this.c.offsetLeft) - this.rect.startX
+        this.rect.h = (e.pageY - this.c.offsetTop) - this.rect.startY
+        this.ctx.clearRect(0, 0, this.c.width, this.c.height)
+        this.ctx.drawImage(this.img, 0, 0)
+        this.draw()
+      }
+    },
+    draw () {
+      this.ctx.strokeRect(this.rect.startX, this.rect.startY, this.rect.w, this.rect.h)
+
+    },
+    canvasLoad () {
+      this.c = document.getElementById('myCanvas')
+      this.ctx = this.c.getContext('2d')
+      this.img = new Image()
+      this.img.src = 'https://wonderfulengineering.com/wp-content/uploads/2016/01/Desktop-Wallpaper-4.jpg'
+      setTimeout(() => {
+        this.ctx.drawImage(this.img, 0, 0)
+        this.shapesCreated.rectangle.forEach(s => {
+          this.drawRectange(s.x, s.y, s.width, s.height)
+        })
+        this.shapesCreated.circle.forEach(s => {
+          this.drawCircle(s.x, s.y, s.radius)
+        })
+      }, 200)
+      console.log(this.shapesCreated)
+    },
     shapeSelector (shape) {
       if (shape === 'square') {
         this.square = true
@@ -108,18 +160,36 @@ export default {
         y: evt.clientY - rect.top
       }
     },
-    drawShapes (x, y) {
-      if (this.square) {
-        this.ctx.rect(x - (this.form.width / 2), y - (this.form.height / 2), this.form.width, this.form.height)
-        this.ctx.stroke()
-      } else if (this.circle) {
-        this.ctx.beginPath()
-        this.ctx.arc(x, y, this.form.radius, 0, 2 * Math.PI)
-        this.ctx.stroke()
-      } else {
+    drawRectange (x, y, width, height) {
+      this.ctx.strokeRect(x, y, width, height)
+      // this.ctx.stroke()
+    },
+    drawCircle (x, y, radius) {
+      this.ctx.beginPath()
+      this.ctx.arc(x, y, radius, 0, 2 * Math.PI)
+      this.ctx.stroke()
+    },
+    saveShapes (x, y, w, h) {
+      // if (this.square) {
+      console.log('this is it')
+      this.shapesCreated.rectangle.push({x: x, y: y, width: w, height: h})
+      this.rect = {}
+      this.canvasLoad()
+      // } else if (this.circle) {
+      //   this.shapesCreated.circle.push({x: x, y: y, radius: this.form.radius})
+      //   // console.log(this.shapesCreated)
+      //   this.canvasLoad()
+      // } else {
 
-      }
+      // }
     }
+    // removeShape () {
+    //   this.shapesCreated.rectangle.pop()
+    //   console.log(this.shapesCreated.rectangle)
+    //   this.canvasLoad()
+    //   // var a = this.shapesCreated.rectangle[this.shapesCreated.rectangle.length - 1]
+    //   // this.ctx.clearRect(a.x, a.y, a.width, a.height)
+    // }
   }
 }
 </script>
